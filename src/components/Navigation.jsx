615844c3a811d48828e641334ffe80c45442dde4
3mpty <aurelien.corre@outlook.fr>
2023-11-16 17:31:01 +0100

    created navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const navLInks = [
+    {
+        name: "projects_",
+        link: "#",
+    },
+    {
+        name: "contact_",
+        link: "#",
+    },
+];
+
+export default function Navigation() {
+    return (
+        <nav>
+            <ul>
+                {navLInks.map((item, id) => (
+                    <li key={id}>
+                        <a href={item.link}>{item.name}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}

1902eedd98fd748c21ef084e1f226d8cef1a2189
3mpty <aurelien.corre@outlook.fr>
2023-11-16 17:16:17 +0100

    Added dependancies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+import Lenis from "@studio-freight/lenis";
+import { ParallaxProvider } from "react-scroll-parallax";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const lenis = new Lenis();
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <ParallaxProvider>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </ParallaxProvider>
+);
